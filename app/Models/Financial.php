536db487781e73ca5a19e5d73da97ed02e8f6f81
Alanna Tanisya Anwar <96564861+alannatnsy@users.noreply.github.com>
2025-12-23 23:35:27 +0700

    feat: add financial model

---
+++ app/Models/Financial.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Financial extends Model
+{
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'user_id',
+        'financial_type',
+        'amount',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
