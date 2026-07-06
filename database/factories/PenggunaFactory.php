e34cebe2bebb28b1d8bf385436150f8bc60ca2e7
dirarmdn <dhiraramadini@gmail.com>
2026-07-06 20:24:24 +0700

    feat: make user code factory logic close to the actual logic

--- database/factories/PenggunaFactory.php
+++ database/factories/PenggunaFactory.php
@@ -18,12 +18,32 @@ class PenggunaFactory extends Factory
     protected static ?string $password;
 
     /**
+     * Sequence for user_code.
+     */
+    protected static ?int $sequence = null;
+
+    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
     public function definition(): array
     {
+        $yymm = date('ym');
+        $prefix = 'KSB' . $yymm;
+
+        if (self::$sequence === null) {
+            $last = User::query()
+                ->where('user_code', 'like', $prefix . '%')
+                ->orderBy('user_code', 'desc')
+                ->value('user_code');
+            
+            self::$sequence = $last ? (int) substr($last, -3) : 0;
+        }
+
+        self::$sequence++;
+        $userCode = $prefix . str_pad((string) self::$sequence, 3, '0', STR_PAD_LEFT);
+
         return [
             'kode_pengguna' => 'KSB' . date('ym') . fake()->unique()->numerify('###'),
             'nik' => fake()->unique()->numerify('################'),
